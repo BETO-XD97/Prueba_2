e885da937fb6a97d637c3744840192b09fc6b77d
roberto rocabado <robertorocabado17@gmail.com>
2024-05-02 01:02:18 -0300

    Segundo Commit

--- Prueba_2/Program.cs
+++ Prueba_2/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Esto es una Prueba de Prueba_2");
+            Console.WriteLine("Esto es una segunda linea de prueba");
         }
     }
 }
