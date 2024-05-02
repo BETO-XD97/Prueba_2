ddf8e3f7a9ca0a37bd6d140fb1bae03208f25ca0
roberto rocabado <robertorocabado17@gmail.com>
2024-05-02 00:57:41 -0300

    PRIMER COMMIT

---
+++ Prueba_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prueba_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esto es una Prueba de Prueba_2");
+        }
+    }
+}
